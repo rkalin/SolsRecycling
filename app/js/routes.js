ce0f7d0720ffdca7604bbe398764d1d010ae0101
rkalin85 <rkalin@gmail.com>
2015-10-04 06:38:43 +0300

    Folder and files updated
    Folder and files updated

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,25 +1,17 @@
 angular.module('recycleTracker').config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
-            .when('/home', {
-                templateUrl: 'partials/home/home.html'
-                //controller: 'View1Ctrl'
-            })
             .when('/register', {
-                templateUrl: 'partials/register/register.html'
-                //controller: 'View2Ctrl'
-            })
-            .when('/signin', {
-                templateUrl: 'partials/signin/signin.html'
+                templateUrl: 'app/partials/register/register.html'
                 //controller: 'View2Ctrl'
             })
             .when('/viewTotal', {
-                templateUrl: 'partials/viewTotal/viewTotal.html'
+                templateUrl: 'app/partials/viewTotal/viewTotal.html'
                 //controller: 'View2Ctrl'
             })
             .when('/viewTotalUser', {
-                templateUrl: 'partials/viewTotalUser/viewTotalUser.html'
+                templateUrl: 'app/partials/viewTotalUser/viewTotalUser.html'
                 //controller: 'View2Ctrl'
             })
-            .otherwise({redirectTo: '/home'});
+            .otherwise({redirectTo: '/viewTotal'});
 }]);
---
+++ app/partials/navigation/navDirective.js
@@ -0,0 +1,5 @@
+angular.module('recycleTracker').directive('myNav', function() {
+   return {
+       templateUrl: 'app/partials/navigation/navigation.html'
+   };
+});
